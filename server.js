3c8b8a9eab331445d43879b3300d5fdcce2f2304
osterhagen <omar.nraza@gmail.com>
2018-03-03 13:16:31 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+/*
+Users:
+GET		/users
+POST	/users
+UPDATE	/users/update
+DELETE	/users/delete
+Notification:
+GET		/notifications
+POST	/notifications
+UPDATE	/notifications/update
+DELETE	/notifications/delete
+Group:
+GET		/group
+POST	/group
+UPDATE	/group/update
+DELETE	/group/delete
+*/
